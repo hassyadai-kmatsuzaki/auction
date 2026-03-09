b248e8c01baebcd2ff68772ecf6081bfd6b7eb84
kmatsuzaki <k.matsuzaki@beer-o-clock.jp>
2026-03-09 17:33:13 +0900

    tidy

--- resources/ts/App.tsx
+++ resources/ts/App.tsx
@@ -23,9 +23,6 @@ import PrivacyPolicy from './pages/legal/PrivacyPolicy';
 import SpecifiedCommercialTransaction from './pages/legal/SpecifiedCommercialTransaction';
 import TermsOfService from './pages/legal/TermsOfService';
 
-// EC（公開ページ・認証不要）
-import EcTop from './pages/ec/EcTop';
-
 // Error pages
 import NotFound from './pages/NotFound';
 
@@ -103,9 +100,6 @@ function App() {
             <Route path="/auth/set-password" element={<SetPassword />} />
             <Route path="/auth/forgot-password" element={<GuestRoute><ForgotPassword /></GuestRoute>} />
             <Route path="/auth/reset-password" element={<GuestRoute><ResetPassword /></GuestRoute>} />
-
-          {/* EC（公開ページ・認証不要） */}
-          <Route path="/ec" element={<EcTop />} />
 
           {/* 法的ページ */}
           <Route path="/legal/privacy" element={<PrivacyPolicy />} />
